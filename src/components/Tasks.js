b225e2f087dd5657ffc6a90ab60daf4f79c3da0e
Francois Lacoursiere <lacouf@gmail.com>
2024-04-14 11:56:05 -0400

    Toggle reminder console

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,15 @@
 import Task from './Task'
-const Tasks = ({tasks, onDelete}) => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         // tasks.push()  ne fonctionne pas puisque tasks est immuable
         // il faut plutot faire la ligne suivante
         // setTasks([...tasks, {}])  utilise le spread operator ...
         <>
             {tasks.map((task) => (
-                <Task key={task.id} task={task} onDelete={onDelete}/>
+                <Task key={task.id}
+                task={task}
+                onDelete={onDelete}
+                onToggle={onToggle}/>
             ))}
         </>
     )
