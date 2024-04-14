3468b076182ce0e752d1178e197663e408951e2e
Francois Lacoursiere <lacouf@gmail.com>
2024-04-14 12:08:50 -0400

    Ajout de useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
@@ -7,6 +7,15 @@ import AddTask from './components/AddTask';
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      console.log(data)
+    }
+    fetchTasks()
+  })
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
