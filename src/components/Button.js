d97479db5d9b57c4d6f3926956affe743c0a709b
Francois Lacoursiere <lacouf@gmail.com>
2024-04-14 11:30:33 -0400

    Passe function du parent a composant Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-    const onClick = () => {
-        console.log('button pressed')
-    }
-
+const Button = ({color, text, onClick}) => {
     return (
         <button
         onClick={onClick}
@@ -21,6 +17,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import Button from './Button'
 
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
         <header className='header'>
           <h1>{title}</h1>
-          <Button color='green' text='Hello'/>
+          <Button color='green' text='Hello' onClick={onClick}/>
         </header>
     )
 }
