ef25a8b2d04335c9606f0929fb4b5ad38913ecec
Francois Lacoursiere <lacouf@gmail.com>
2024-04-14 11:59:55 -0400

    Ajout dans le state de App le nouveau task

--- src/App.js
+++ src/App.js
@@ -29,6 +29,12 @@ function App() {
         ]
   )
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -46,7 +52,7 @@ function App() {
   return (
     <div className='container'>
       <Header/>
-      <AddTask/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ?
        <Tasks tasks={tasks}
        onDelete={deleteTask}
