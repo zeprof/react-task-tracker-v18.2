863106655425d58859cee4876fc795866d3ac79c
Francois Lacoursiere <lacouf@gmail.com>
2024-04-14 11:26:37 -0400

    Composant Bouton reutilisable

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ const Header = ({title}) => {
     return (
         <header className='header'>
           <h1>{title}</h1>
-          <Button />
+          <Button color='green' text='Hello'/>
+          <Button color='red' text='Allo'/>
+          <Button color='blue' text='React'/>
         </header>
     )
 }
