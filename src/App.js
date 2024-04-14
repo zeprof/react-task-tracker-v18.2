c34776253b2b07a1ffe80b2bab173dad729f2bbe
Francois Lacoursiere <lacouf@gmail.com>
2024-04-14 11:11:40 -0400

    Premier composant React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 
 import './App.css';
+import Header from './components/Header'
 
 function App() {
-  const name = 'Francois'
-  const x = false
-
   return (
-    <>
-      <h1>Hello from React</h1>
-      <h2>Allo {name}</h2>
-      <h2>Calcul {1 +1}</h2>
-      <h2>Conditionnel {x ? 'vrai' : 'faux'}</h2>
-    </>
+    <div className='container'>
+      <Header/>
+    </div>
   );
 }
 
